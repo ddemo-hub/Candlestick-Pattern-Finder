76ea10fb8a875141c5f84c50e9dcf026db19d495
dem0-0 <85748316+dem0-0@users.noreply.github.com>
2022-05-10 17:43:19 +0300

    Update Pattern_Finder.py

--- Pattern_Finder.py
+++ Pattern_Finder.py
@@ -4,6 +4,8 @@ import datetime
 import requests
 import json
 
+url = "https://api.binance.com/api/v3/klines"
+
 """
 input:   Python dictionary containing the parameters for the Binance API
 returns: Pandas dataframe that maps the requested kline data  
@@ -72,10 +74,46 @@ def getShootingStarDataFrame(ogDF):
     return apdf
 
 """
+input:  Dataframe holding the kline data
+output: Python list holding the timestamps in which a bullish engulf formation is present at
+"""    
+def markBullishEngulfTimestamps(klineDF):
+    timestamps = []
+    for x in range(0,len(klineDF)):
+        if klineDF.iloc[x]['Open'] < klineDF.iloc[x]['Close']:
+            if klineDF.iloc[x]['Close'] > klineDF.iloc[x-1]['Open'] and klineDF.iloc[x-1]['Close'] < klineDF.iloc[x-1]['Open']:
+                if (((klineDF.iloc[x-1]['Open'] - klineDF.iloc[x-1]['Close']) / klineDF.iloc[x-1]['Open']) * 100) >= 0.5:
+                    timestamps.append(klineDF.index[x])
+    
+    return timestamps
+
+"""
+input:   Original dataframe
+returns: Pandas dataframe to be used to initialize an additional plot  
+"""
+def getBullishEngulfDataFrame(ogDF):
+    timestamps = markBullishEngulfTimestamps(ogDF)
+    ap1 = [float('nan')]*len(ogDF)
+    ap2 = [float('nan')]*len(ogDF)
+    
+    apdfO = pd.DataFrame(dict(point=ap1),index=ogDF.index)
+    apdfC = pd.DataFrame(dict(point=ap2),index=ogDF.index)
+    
+    for x in timestamps:
+        apdfO.loc[x] = ogDF.loc[x]['Open']
+    
+    for x in timestamps:
+        apdfC.loc[x] = ogDF.loc[x]['Close'] 
+    
+    apdfs = [apdfO, apdfC]
+    
+    return apdfs    
+                
+"""
 input:  Original dataframe, Dataframe for the initialization of the additional plot
-output: Plots the chart
+output: Plots the shooting star chart
 """   
-def plotMarkedChart(ogDF, apDF):
+def plotSSChart(ogDF, apDF):
     ap = mpf.make_addplot(apDF, type='scatter', markersize=200, marker='.', color='#FFFF00')
     
     mpf.plot(
@@ -85,20 +123,17 @@ def plotMarkedChart(ogDF, apDF):
     addplot = ap
     )
 
-
-def main():
-    print("Enter the symbol:", end = " ")
-    symb = str(input())    
-    print("Enter the interval:", end = " ")
-    intrvl = str(input())
+"""
+input:  Original dataframe, Dataframe for the initialization of the additional plot
+output: Plots the bullish engulf chart
+"""   
+def plotBEChart(ogDF, apDF):
+    aps = [mpf.make_addplot(apDF[0], type='scatter', markersize=100, marker='^', color='#FFFF00'),
+           mpf.make_addplot(apDF[1], type='scatter', markersize=100, marker='v', color='#FFFF00')]
     
-    parameters = {'symbol': symb, 'interval':intrvl}
-    df = getKlineData(parameters)
-
-    ssdf = getShootingStarDataFrame(df)
-
-    plotMarkedChart(df, ssdf)
-    
-    
-url = "https://api.binance.com/api/v3/klines"
-main()
+    mpf.plot(
+    ogDF,
+    type = 'candle',
+    style = 'yahoo',
+    addplot = aps
+    )
